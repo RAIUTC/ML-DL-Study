3529992746955a736ab075d9f029b65e4efc9a63
raiutc <syi05003@gmail.com>
2024-07-20 00:15:57 +0900

    nothing

--- spatial_locality.py
+++ spatial_locality.py
@@ -13,7 +13,6 @@ def conv(image, filter, threshold):
 
 # 이미지 사이즈가 n,n이고 filter 사이즈가 k,k
 # 각 k,k크기대로 대응하는 픽셀의 값을 곱해 k,k사이즈의 픽셀들 합을 구해야함
-    
 
 # 비교해서 threshold값을 넘기면 그 위치의 k,k를 반환해야함
     for i in image:
